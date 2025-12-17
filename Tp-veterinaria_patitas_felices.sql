c775f6f23aa38ba54dbbbc756424624121074750
peshiito <pgbaez0@gmail.com>
2025-12-16 23:19:23 -0300

    update punto 8 hay que solucionar ya

--- Tp-veterinaria_patitas_felices.sql
+++ Tp-veterinaria_patitas_felices.sql
@@ -153,3 +153,47 @@ set descripcion = "Limpieza de pulgas"
 where id = 2;
 
 -- Ejercicio 8
+
+delete from duenos
+where id = 3;
+
+delete from duenos
+where nombre = "Pedro" and apellido = "Baez";
+
+alter table historial_clinico drop FOREIGN KEY historial_clinico_ibfk_1;
+
+
+alter table historial_clinico 
+add FOREIGN KEY (id_mascota) REFERENCES mascotas(id) ON DELETE CASCADE;
+
+delete from mascotas 
+where id = 1;
+
+-- Como ver los historiales pero n se deberia ver el eliminado
+select * from historial_clinico;
+
+
+-- Cargo nuevos datos para que no quede vacia la base de datos
+INSERT into duenos (nombre, apellido, telefono, direccion)
+values ('Ana', 'Martinez', '1122334455', 'Calle Falsa 123'),
+         ('Luis', 'Rodriguez', '5566778899', 'Avenida Siempre Viva 742'),
+        ('Maria', 'Gonzalez', '9988776655', 'Boulevard Central 456');
+        ('Carlos', 'Lopez', '4433221100', 'Camino Real 789');
+
+INSERT into mascotas (nombre, especie, fecha_nacimiento, id_dueno)
+values ('Rex', 'Perro', '2020-05-10', 4 ),
+       ('Mimi', 'Gato', '2019-08-15', 5),
+       ('Buddy', 'Perro', '2021-11-20', 6),
+       ('Luna', 'Gato', '2018-03-25', 7);
+
+INSERT into veterinarios (nombre, apellido, matricula, especialidad)
+values ('Sofia', 'Fernandez', '112233', 'Perros'),  
+       ('Diego', 'Sanchez', '445566', 'Gatos'),
+       ('Elena', 'Ramirez', '778899', 'Animales Exóticos'),
+       ('Javier', 'Torres', '990011', 'Aves');
+
+INSERT into historial_clinico (id_mascota, id_veterinario, descripcion)
+values (2, 1, 'Vacunación anual'),
+       (3, 2, 'Chequeo general'),
+       (4, 3, 'Tratamiento antipulgas'),
+       (1, 4, 'Revisión postoperatoria');
